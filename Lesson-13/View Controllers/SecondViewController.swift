823e857e736e50a35c77d6ac313abf48b81e0dcc
Kamalpreet Singh <kamalpreetsingh@kamalpreets-mac.local>
2018-04-23 15:53:06 -0700

    Added 3 ViewControllers

---
+++ Lesson-13/View Controllers/SecondViewController.swift
@@ -0,0 +1,20 @@
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
+    
+}
+
--- Lesson-13/ViewController.swift
+++ Lesson-13/ViewController.swift
@@ -1,23 +1,9 @@
-//
-//  ViewController.swift
-//  Lesson-13
-//
-//  Created by Kamalpreet Singh on 2018-04-23.
-//  Copyright © 2018 on. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
